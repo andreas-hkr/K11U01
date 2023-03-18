0ac3b20923ada4b28761ff7d24275398dd0a22de
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 08:42:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in max 1000 tal från användaren och skriv sen ut dem, men skriv bara ut varje tal en gång
+
+
+    }
+}
